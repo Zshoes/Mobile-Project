2974a3ba8097bc1cf8c7652cef238234be5b018e
Zshoes <70231318+Zshoes@users.noreply.github.com>
2025-01-12 21:33:19 -0700

    added turret obj

---
+++ Assets/Code/Scripts/Turret.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void Attack()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
