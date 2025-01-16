5f520c624973941afcc5492f9870e5588700387c
Zshoes <70231318+Zshoes@users.noreply.github.com>
2025-01-15 17:23:57 -0700

    building turrets

--- Assets/Code/Scripts/Turret.cs
+++ Assets/Code/Scripts/Turret.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private float attackSpeed;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
