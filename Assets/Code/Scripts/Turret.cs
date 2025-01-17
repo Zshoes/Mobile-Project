7483072214ed06cda5a026a561b5086988ec32d3
Zshoes <70231318+Zshoes@users.noreply.github.com>
2025-01-16 17:20:41 -0700

    turret aims towards enemies as they pass

--- Assets/Code/Scripts/Turret.cs
+++ Assets/Code/Scripts/Turret.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
 
 public class Turret : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private float attackSpeed;
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
 
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 3f;
+    [SerializeField] private float rotationSpeed = 200f;
 
+    private Transform target; 
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,13 +26,44 @@ public class Turret : MonoBehaviour
         
     }
 
-    private void Attack()
-    {
-
-    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if(hits.Length > 0)
+        {
+            target = hits[0].transform; 
+        }
+    }
+
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
