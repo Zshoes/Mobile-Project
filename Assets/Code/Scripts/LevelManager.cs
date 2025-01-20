e8057cfbbf964a70367fa28dbd282830d298be2f
Zshoes <70231318+Zshoes@users.noreply.github.com>
2025-01-20 12:20:52 -0700

    added currency

--- Assets/Code/Scripts/LevelManager.cs
+++ Assets/Code/Scripts/LevelManager.cs
@@ -7,8 +7,35 @@ public class LevelManager : MonoBehaviour
     public Transform[] path;
     public Transform startPoint;
 
+    public int currency;
+
+
+    private void Start()
+    {
+        currency = 100; //starting cash
+    }
+
     private void Awake()
     {
         main = this;
+    }
+
+    public void IncreaseCurrency(int amount)
+    {
+        currency += amount;
+    }
+
+    public bool SpendCurrency(int amount)
+    {
+        if (amount <= currency) {
+            //buying items
+            currency -= amount;
+            return true;
+        }
+        else
+        {
+            Debug.Log("Too Poor to Buy"); //prolly change to a ui pop up
+            return false;
+        }
     }
 }
