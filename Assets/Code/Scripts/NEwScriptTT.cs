d4f37e615cb8fd03ca2f53b316df783b81380e3a
Zshoes <70231318+Zshoes@users.noreply.github.com>
2025-01-22 18:30:19 -0700

    set up framework for new turrets

---
+++ Assets/Code/Scripts/NEwScriptTT.cs
@@ -0,0 +1,24 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NEwScriptTT : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("References")]
+    [SerializeField] public float attackTime = 5f;
+
+    private void TestTT()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
