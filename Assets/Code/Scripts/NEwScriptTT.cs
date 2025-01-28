e2fc85757801e39fea29071f46936877da6f4617
Zshoes <70231318+Zshoes@users.noreply.github.com>
2025-01-27 18:53:25 -0700

    light upkeep, future changes laid out

--- Assets/Code/Scripts/NEwScriptTT.cs
+++ Assets/Code/Scripts/NEwScriptTT.cs
@@ -7,13 +7,21 @@ public class NEwScriptTT : MonoBehaviour
     [Header("References")]
     [SerializeField] public float attackTime = 5f;
     [SerializeField] public float attackDamage = 2f;
-    [SerializeField] public float health = 50f;
+    [SerializeField] public float health = 50f; 
+    [SerializeField] public float gateHealth = 500f;
+
 
 
     [Header("Attributes")]
     [SerializeField] private float speed = 5f;
     [SerializeField] private int dmg = 1;
 
+    /**notes for later additions 
+     * add gate that the enemies attack instead of just crossing a finsih line
+     * give the "Gate" HP points that the enimes hit as they wait
+     * add gate defeneses you can buy / upggrade
+     * make enemies that are "special" i.e enemies that only certain towers can attack
+     * add eniems that attack back at the towers, potentially killing them? (needs to be tested) **/
     private void TestTT()
     {
 
